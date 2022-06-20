197e2e8f0ebca02678e5a56c7348138b848213e3
Aman152001 <amanpyadav15@gmail.com>
2022-06-20 19:37:27 -0400

    Finished with second javascript Exercise

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-//Call Hello World with alert!
-function HelloWorld(){
-    alert("Hello World!");
+//get the string from the user
+function getString(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+//Reverse the string
+function reverseString(userString){
+
+    let rev = [];
+    for(let i=userString.length-1;i>=0;i--){
+        rev +=  userString[i];
+    }
+    return rev;
+}
+
+//Display the reversed string to the user
+function displayString(revString){
+    document.getElementById("msg").innerHTML=`Your reversed string is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
